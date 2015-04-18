a8ca95249e55c17f3cc18dd9945bc0f152b43776
ThomasHedegaard <thomashj93@gmail.com>
2015-04-18 21:46:45 +0200

    init

---
+++ js/node.js
@@ -0,0 +1,13 @@
+var Node = function(_argName){
+    // Collection of neighboring nodes, will be edges
+    var neighbours = [];
+    var argName = _argName;
+    var minDistance = Infinity; // TODO: change this at some point
+    var previousNode;
+
+
+
+
+
+    return{};
+};

15ee7e29bfbc23b26ec7c6f327bf58ba050875bc
ThomasHedegaard <thomashj93@gmail.com>
2015-04-19 11:29:10 +0200

    Test added for node

--- tests/node_test.js
+++ tests/node_test.js
@@ -1,15 +1,33 @@
 describe("a node", function () {
 
     var node;
+    // Params for node
+    var nameOnNode = "nodeName";
 
     beforeEach(function () {
-        node = new Node("name on node");
+        node = new Node(nameOnNode);
     });
 
     it("should be defined", function () {
-        console.log("Should be defined");
         expect(node).toBeDefined();
     });
 
+    it("should have the same name", function () {
+        expect(node.name).toEqual(nameOnNode);
+    });
+
+    it("should have neighbours", function () {
+
+        var neighbourTest = [
+            new Edge(new Node("test1"), 7),
+            new Edge(new Node("test2"), 9),
+            new Edge(new Node("test3"), 14)
+        ];
+        node.neighbours = neighbourTest;
+
+        expect(typeof node.neighbours).toBe('object');
+        expect(node.neighbours.length).toBe(neighbourTest.length);
+        expect(node.neighbours).toEqual(neighbourTest);
+    });
 
 });
